4a797676a373a2f1a8e3f8da5a3023d3db50ea1d
Gabriel Carneiro <carneiro.gabr@gmail.com>
2021-09-25 21:13:42 -0300

    card stats

--- src/model/Card.py
+++ src/model/Card.py
@@ -1,14 +1,25 @@
+from typing import Optional
+
 from .Color import Color
 
 
 class Card:
-    def __init__(self, value: int, color: Color, is_buy_card: bool = False) -> None:
-        self._value: int = value
+    def __init__(self,
+                 color: Color,
+                 value: Optional[int] = None,
+                 is_buy_card: bool = False,
+                 is_reverse: bool = False,
+                 is_block: bool = False,
+                 is_change_color: bool = False) -> None:
+        self._value: Optional[int] = value
         self._color: Color = color
+        self._is_change_color: bool = is_change_color
         self._is_buy_card: bool = is_buy_card
+        self._is_reverse: bool = is_reverse
+        self._is_block: bool = is_block
 
     def get_color(self) -> Color:
         return self._color
 
-    def get_value(self) -> int:
+    def get_value(self) -> Optional[int]:
         return self._value
--- src/model/Table.py
+++ src/model/Table.py
@@ -1,14 +1,16 @@
-from typing import List
+from typing import List, Dict
 
 from .Card import Card
+from .Color import Color
 from .Player import Player
 
 
 class Table:
     def __init__(self) -> None:
         self._players: List[Player] = []
-        self._buy_cards: int = 0
-        self._top_card: Card = None     # noqa # FIXME
+        self._value_to_buy: int = 0
+        self._deck: Dict[List[Card]] = {}   # FIXME: keys
+        self._top_card: Card = None         # FIXME: type
         self._reverse: bool = False
         self._num_players: int = len(self._players)
 
@@ -23,6 +25,18 @@ class Table:
         self._players.remove(player)
         return None
 
+    def _set_deck(self) -> None:
+        for color in Color:
+            for value in range(10):
+                card = Card(value=value, color=color)
+                self._deck["NORMAL"].append(card)
+
+            plus_two: Card = Card(value=2, color=color)
+            reverse: Card = Card(color=color, is_reverse=True)
+            block: Card = Card(color=color, is_block=True)
+
+        return None
+
     def _next_player(self, actual_player: Player) -> Player:
         index: int = self._players.index(actual_player)
         if self._reverse:
@@ -32,5 +46,6 @@ class Table:
 
         return self._players[index + 1]
 
-    def is_allowed_letter(self, card: Card) -> bool:
-        pass
+    def allowed_cards(self, card: Card) -> bool:
+        if self._value_to_buy:
+            return True
